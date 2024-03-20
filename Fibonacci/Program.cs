21b5a7ad0fec82797281c68ecc5fba58c865a42d
Thiago Freitas <125612260+thfreitas11@users.noreply.github.com>
2024-03-20 16:01:10 -0300

    Sequência de Fibonacci

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+{
+    int primeiroNumero = 0;
+    int segundoNumero = 1;
+    int proximoNumero = 0;
+    int termo;
+
+    Console.WriteLine("Digite número de termos para Sequência de Fibonacci:");
+    termo = Convert.ToInt32(Console.ReadLine());
+
+        
+    Console.Write("{0},{1}", primeiroNumero, segundoNumero);
+    
+    for (int i = 2; i < termo; i++)
+    {
+        proximoNumero = primeiroNumero + segundoNumero;
+        
+
+        Console.Write(",{0}", proximoNumero);
+
+        primeiroNumero = segundoNumero;
+        segundoNumero = proximoNumero;
+
+    }
+
+  
+}
