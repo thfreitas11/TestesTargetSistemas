1a8f5959b251d092cea83717d961eb03fc8b743b
Thiago Freitas <125612260+thfreitas11@users.noreply.github.com>
2024-03-20 16:02:18 -0300

    Invertendo String

---
+++ InvertendoString/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+string texto;
+
+Console.WriteLine("Digite um texto ou palavra:");
+texto = Console.ReadLine();
+
+Console.WriteLine("Texto Normal: " + texto);
+
+string textoInvertido = new string(texto.Reverse().ToArray());
+
+Console.WriteLine("Texto Invertido: " + textoInvertido);
